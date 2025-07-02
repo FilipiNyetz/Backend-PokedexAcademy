bf7dbbda6eba785cc3c12b52dbfa6b056d26253c
FilipiNyetz <romao.martins.filipi@gmail.com>
2025-07-02 17:02:48 -0300

    Remove import upload

--- src/app.js
+++ src/app.js
@@ -1,17 +1,8 @@
 import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
 import routes from './routes/index.js';
 
 const app = express();
 app.use(express.json());
-
-// Suporte a __dirname com ESModules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Servir arquivos estáticos da pasta "uploads"
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Rota raiz para health check
 app.get('/', (req, res) => {
