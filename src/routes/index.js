d81d7a8fb73d8ce409cbca6377cc6bd13d492918
Filipi Romão <129067530+FilipiNyetz@users.noreply.github.com>
2025-07-01 22:41:40 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import userRoutes from './user.route.js'
-import positionRoutes from './position.route.js'
+import userRoutes from './user.route.js';
+import positionRoutes from './position.route.js';
+import uploadRoutes from './upload.route.js';
 
 const router = express.Router();
 
-router.use('/user', userRoutes)
-router.use('/position', positionRoutes)
+router.use('/user', userRoutes);
+router.use('/position', positionRoutes);
+router.use('/upload', uploadRoutes);
 
-export default router
+export default router;
